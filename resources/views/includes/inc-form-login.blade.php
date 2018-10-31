62136de88738aced6b708b19f2bc70b97dd08b25
Jack Bodman <jack.bodman1998@outlook.com>
2018-10-31 14:21:14 +0000

    Frontend Form and UI Improvements
    Updated forms to clear value on focus. Also set password inputs up to cover text input.

--- resources/views/includes/inc-form-login.blade.php
+++ resources/views/includes/inc-form-login.blade.php
@@ -1,7 +1,7 @@
 <div>
   <form action="login.php" method="post">
-    <input type="text" name="email" value="Email Address">
-    <input type="text" name="password" value="Password">
+    <input type="text" name="email" onfocus="this.value=''" value="Email Address">
+    <input type="password" name="password" onfocus="this.value=''" value="Password">
     <button type="submit" name="submit">Login</button>
   </form>
 </div>
--- resources/views/includes/inc-form-signup.blade.php
+++ resources/views/includes/inc-form-signup.blade.php
@@ -1,9 +1,9 @@
 <div>
   <form action="signup.php" method="post">
-    <input type="text" name="username" value="Username">
-    <input type="text" name="email" value="Email Address">
-    <input type="text" name="password" value="Password">
-    <input type="text" name="conf-password" value="Confirm Password">
+    <input type="text" name="username" onfocus="this.value=''" value="Username">
+    <input type="text" name="email" onfocus="this.value=''" value="Email Address">
+    <input type="text" name="password"  onfocus="this.type='password',this.value=''" value="Password">
+    <input type="text" name="conf-password" onfocus="this.type='password',this.value=''" value="Confirm Password">
     <button type="submit" name="submit">Sign Up</button>
   </form>
 </div>
