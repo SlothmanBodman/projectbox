83508df7480979a9e622995b51b00d64634616f7
Jack Bodman <jack.bodman1998@outlook.com>
2018-11-01 21:27:04 +0000

    Password Hot fix

--- resources/views/includes/inc-form-login.blade.php
+++ resources/views/includes/inc-form-login.blade.php
@@ -1,7 +1,7 @@
 <div>
   <form action="login.php" method="post">
     <input type="text" name="email" onfocus="this.value=''" value="Email Address">
-    <input type="password" name="password" onfocus="this.value=''" value="Password">
+    <input type="text" name="password" onfocus="this.type='password',this.value=''" value="Password">
     <button type="submit" name="submit">Login</button>
   </form>
 </div>
