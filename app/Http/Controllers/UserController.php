6de405e59db1f7e7b85a2e0b5e054b5dc3e308c3
Jack Bodman <jack.bodman1998@outlook.com>
2018-11-20 20:20:41 +0000

    Minor Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function newUser(Request $request)
-    {
-      
-    }
+  public function newUser(Request $request)
+  {
+    $this->validate($request, [
+      'username'=>'required',
+      'email'=>'required',
+      'password'=>'required',
+      'conf-password'=>'required'
+    ]);
+    //create user
+    $user = new User;
+    $user->username = $request->input('username');
+    $user->email = $request->input('email');
+    $user->password = $request->input('password');
+  }
 }
--- resources/views/includes/inc-form-signup.blade.php
+++ resources/views/includes/inc-form-signup.blade.php
@@ -1,9 +1,9 @@
 <div>
-  <form action="{{ action("UserController@newUser") }}" method="post">
+  <form action="{{ action("UserModel@newUser") }}" method="post">
     <input type="text" name="username" placeholder="Username">
-    <input type="text" name="email" placeholder="Email Address">
-    <input type="text" name="password"  onfocus="this.type='password'" placeholder="Password">
-    <input type="text" name="conf-password" onfocus="this.type='password'" placeholder="Confirm Password">
+      <input type="text" name="email" placeholder="Email Address">
+        <input type="text" name="password"  onfocus="this.type='password'" placeholder="Password">
+      <input type="text" name="conf-password" onfocus="this.type='password'" placeholder="Confirm Password">
     <button type="submit" name="submit">Sign Up</button>
   </form>
 </div>
